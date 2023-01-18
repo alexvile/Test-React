fc3a829544db4d4b56f5767d17e62bc62866710a
Alexey <alexei20293@yahoo.com>
2023-01-18 19:53:02 +0200

    repeat 1 and 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
+import user from '../Data/user.json';
+import dataStat from '../Data/data.json';
+
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics stats={dataStat} />
     </div>
   );
 };
