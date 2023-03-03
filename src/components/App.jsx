974b0d103ee23e773a3d10c3b8deb657faf6eae3
Alexei Solotvinskiy <alexei20293@yahoo.com>
2023-03-03 15:58:39 +0200

    created search form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
+import { SearchFrom } from "./SearchForm";
+
 export const App = () => {
+  const handleFormSubmit = query => {
+    console.log(query)
+  };
   return (
     <div>
       <h1>Test</h1>
+      <SearchFrom onSubmit={handleFormSubmit}/>
     </div>
   );
 };
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export const SearchFrom = ({onSubmit}) => {
+    const [query, setQuery] = useState('');
+    const handleChange = e => {
+        setQuery(e.currentTarget.value.toLowerCase());
+    };
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(query === '') {
+            return alert('empty query');
+        }
+        onSubmit(query);
+    }
+    return(
+        <form onSubmit={handleSubmit} className="p-2 border">
+            <label>
+                <input onChange={handleChange} className="border" type="text" placeholder="Enter query" value={query}/>
+            </label>
+            <button type="submit" className="bg-red-200"> search</button>
+        </form>
+    )
+}
